f569807765c8b1ad53fbea5839b3a9aa7313de98
Supreet Singh <supreetsingh106@gmail.com>
2019-11-25 11:31:13 +0530

    inintialize

---
+++ src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.fs101.writerpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriterpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xebia/fs101/writerpad/WriterpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xebia.fs101.writerpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriterpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
