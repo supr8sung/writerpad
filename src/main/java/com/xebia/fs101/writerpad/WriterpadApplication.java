26c1bf3526aed31e713ec56a69c2d828f21a65c8
Supreet Singh <supreetsingh106@gmail.com>
2019-11-25 13:03:25 +0530

    circleci added

--- src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
+++ src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WriterpadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WriterpadApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(WriterpadApplication.class, args);
+    }
 }
