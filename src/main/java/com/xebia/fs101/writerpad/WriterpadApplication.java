4faee8091afe6d8b27c5763f58c44b947e63298b
Supreet Singh <supreetsingh106@gmail.com>
2019-12-07 02:23:13 +0530

    heroku testing

--- src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
+++ src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
@@ -1,12 +1,35 @@
 package com.xebia.fs101.writerpad;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableAsync
 public class WriterpadApplication {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Bean
+    public DataSource dataSource() {
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(dbUser);
+        config.setPassword(dbPassword);
+        return new HikariDataSource(config);
+    }
+
     public static void main(String[] args) {
 
         SpringApplication.run(WriterpadApplication.class, args);
